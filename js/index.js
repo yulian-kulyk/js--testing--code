3d57dd80cbc528e5381e38a9204fb712b5feb26f
Yulian Kulyk <ylian.game07@gmail.com>
2023-08-20 20:40:34 +0300

    6 kyu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 
+console.log(number([[10,0],[3,5],[5,8]]));
+
+
+function number (busStops) {
+    let result = 0;
+    for (let i = 0; i < busStops.length; i++) {
+        result += busStops[i][0];
+        result -= busStops[i][1];
+    }
+    return result;
+}
--- js/tasks.js
+++ js/tasks.js
@@ -142,6 +142,18 @@ function paperwork(n, m) {
   }
   
 
+console.log(number([[10,0],[3,5],[5,8]]));
+
+
+function number (busStops) {
+    let result = 0;
+    for (let i = 0; i < busStops.length; i++) {
+        result += busStops[i][0];
+        result -= busStops[i][1];
+    }
+    return result;
+}
+//kata
 
 
 
