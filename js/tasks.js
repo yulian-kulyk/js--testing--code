485637633473ea117ed3af4c8b3d251703fced2a
Yulian Kulyk <ylian.game07@gmail.com>
2023-08-22 23:21:18 +0300

    kata

--- js/tasks.js
+++ js/tasks.js
@@ -180,6 +180,22 @@ function domainName(url){
 
 Math.floor(5.95);
 
+//console.log(isIsogram("Dermatoglyphics"));
+console.log(isIsogram("aba"));
+
+function isIsogram(str) {
+  str = str.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+
 
 
 
