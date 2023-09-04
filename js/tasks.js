d877ea6abb92f0bea37e430a101d667f74e5fc43
Yulian Kulyk <ylian.game07@gmail.com>
2023-09-04 17:44:32 +0300

    kata

--- js/tasks.js
+++ js/tasks.js
@@ -326,7 +326,12 @@ function expandedForm(num) {
     return expandedDigits.join(' + ');
   }
 
-
+function feast(beast, dish) {
+    if(beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]){
+        return true;
+    }
+    return false;
+}
 
 
 
