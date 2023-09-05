9151fa55115141fd1c0ea0c83e5730892c8da109
Yulian Kulyk <ylian.game07@gmail.com>
2023-09-05 22:00:14 +0300

    kata

--- js/tasks.js
+++ js/tasks.js
@@ -336,7 +336,21 @@ function smash (words) {
    return words.join(' ')
 };
 
+function getGrade(s1, s2, s3) {
+  const average = (s1 + s2 + s3) / 3;
 
+  if (average >= 90 && average <= 100) {
+    return 'A';
+  } else if (average >= 80 && average < 90) {
+    return 'B';
+  } else if (average >= 70 && average < 80) {
+    return 'C';
+  } else if (average >= 60 && average < 70) {
+    return 'D';
+  } else {
+    return 'F';
+  }
+}
 
 
 
