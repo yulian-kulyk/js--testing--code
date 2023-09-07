ed6bc8b2c296ad958828851fa68f6c5d14c1da4b
Yulian Kulyk <ylian.game07@gmail.com>
2023-09-07 21:48:09 +0300

    kata

--- js/tasks.js
+++ js/tasks.js
@@ -372,7 +372,9 @@ function basicOp(operation, value1, value2)
 function checkForFactor (base, factor) {
   return (base % factor) ? false : true;
 }
-
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return ((fuelLeft * mpg) >= distanceToPump) ? true : false;
+};
 
 
 
