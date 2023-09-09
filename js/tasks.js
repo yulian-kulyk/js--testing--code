a7a931792a3503c097639300c1570835982f6db1
Yulian Kulyk <ylian.game07@gmail.com>
2023-09-09 13:11:08 +0300

    kata

--- js/tasks.js
+++ js/tasks.js
@@ -391,7 +391,17 @@ function wave(str) {
     return result;
 }
 
-
+function points(games) {
+    var sum=0;
+    for (var i=0; i<games.length; ++i)
+    {
+      if (games[i][0]>games[i][2])
+        sum+=3;
+      if (games[i][0]==games[i][2])
+        sum+=1;
+    }
+    return sum;
+  }
 
 
 
